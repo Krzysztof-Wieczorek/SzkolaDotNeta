623ba413b03e9c6483e2da09fecfe627c1857f99
Krzysztof Wieczorek <krzysztof.wieczorek@rulandec.com>
2022-12-07 20:56:27 +0100

    Created first project - Hello world.

---
+++ Week01_Lesson07_Ex01_HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Week01_Lesson07_Ex01_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
