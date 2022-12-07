ee2ec4cc7e643bac94b0bfe3aaf75a208d1b2dd6
Krzysztof Wieczorek <krzysztof.wieczorek@rulandec.com>
2022-12-07 20:46:16 +0100

    Add project files.

---
+++ 01_07_01_HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_07_01_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
